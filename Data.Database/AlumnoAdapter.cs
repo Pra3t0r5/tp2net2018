164dddca231b27d0d78e39b0d342d23e06d3fb42
street.6 <street.6@hotmail.com>
2021-03-11 19:32:03 -0300

    Actualizado profesor y alumno adapter con SP

--- Data.Database/AlumnoAdapter.cs
+++ Data.Database/AlumnoAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Entities;
+using Data.Database.context;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -13,75 +14,71 @@ namespace Data.Database
     {
         public List<Persona> GetAll()
         {
-            List<Persona> alumnos = new List<Persona>();
-            try
+            using (var context = new AcademiaContext())
             {
-                this.OpenConnection();
-                SqlCommand cmdAlumnos = new SqlCommand("SELECT * FROM PERSONAS where tipo_persona = 2",SqlConn);
-                SqlDataReader drAlumnos = cmdAlumnos.ExecuteReader();
-                while(drAlumnos.Read())
-                {
-                    Persona psr = new Persona();
-                    psr.ID = (int)drAlumnos["id_persona"];
-                    psr.Nombre = (string)drAlumnos["nombre"];
-                    psr.Apellido = (string)drAlumnos["apellido"];
-                    psr.Direccion = drAlumnos["direccion"] != DBNull.Value? (string)drAlumnos["direccion"] : null;
-                    psr.Email = drAlumnos["email"] != DBNull.Value ? (string)drAlumnos["email"] : null ;
-                    psr.Telefono = drAlumnos["telefono"] != DBNull.Value ? (string)drAlumnos["telefono"] : null;
-                    psr.FechaNacimiento = (DateTime)drAlumnos["fecha_nac"];
-                    psr.Legajo = drAlumnos["legajo"] != DBNull.Value? (int)drAlumnos["legajo"] : 0;
-                    psr.TipoPersona = drAlumnos["tipo_persona"] != DBNull.Value ? (int)drAlumnos["tipo_persona"] : 0;
-                    psr.IDPlan = (int)drAlumnos["id_plan"];
-                    alumnos.Add(psr);
-                }
-                drAlumnos.Close();
-                this.CloseConnection();
-                return alumnos;
-            }
-            catch(Exception Ex)
-            {
-                Exception ExcepcionControlada = new Exception("Error al traer datos de alumnos",Ex);
-                throw ExcepcionControlada;
+                var parametros = "@TipoPersona=2";
+                return context.Database.SqlQuery<Persona>($"PersonaGetAll {parametros}").ToList();
             }
 
         }
 
         public Persona GetOne(int ID)
         {
-            Persona psr = new Persona();
-            try
+            using (var context = new AcademiaContext())
             {
-                this.OpenConnection();
-                SqlCommand cmdUsuario = new SqlCommand("SELECT * FROM PERSONAS WHERE id_persona=@id and tipo_persona = 2",SqlConn);
-                cmdUsuario.Parameters.Add("@id", SqlDbType.Int).Value = ID;
-                SqlDataReader drAlumnos = cmdUsuario.ExecuteReader();
-                if (drAlumnos.Read())
-                {
-                    
-                    psr.ID = (int)drAlumnos["id_persona"];
-                    psr.Nombre = (string)drAlumnos["nombre"];
-                    psr.Apellido = (string)drAlumnos["apellido"];
-                    psr.Direccion = (string)drAlumnos["direccion"];
-                    psr.Email = (string)drAlumnos["email"];
-                    psr.Telefono = (string)drAlumnos["telefono"];
-                    psr.FechaNacimiento = (DateTime)drAlumnos["fecha_nac"];
-                    psr.Legajo = (int)drAlumnos["legajo"];
-                    psr.TipoPersona = (int)drAlumnos["legajo"];
-                    psr.IDPlan = (int)drAlumnos["id_plan"];
-                }
-                drAlumnos.Close();
+                var parametros = $"@Id={ID}";
+                return context.Database.SqlQuery<Persona>($"PersonaGetOne {parametros}").FirstOrDefault();
             }
-            catch(Exception Ex)
-            {
-                Exception ExcepcionControlada = new Exception("Error al recuperar alumno", Ex);
-                throw ExcepcionControlada;
-            }
-            finally
-            {
-                this.CloseConnection();
-            }
-            return psr;
         }
+
+        public void Delete(int ID)
+        {
+            using (var context = new AcademiaContext())
+            {
+                var parametros = $"@Id={ID}";
+                context.Database.SqlQuery<Persona>($"PersonaDelete {parametros}").FirstOrDefault();
+            }
+        }
+
+        protected void Update(Persona psr)
+        {
+            using (var context = new AcademiaContext())
+            {
+                var parametros = $"@Id={psr.ID},@Nombre='{psr.Nombre}', @Apellido='{psr.Apellido}', @Direccion='{psr.Direccion}'," +
+                    $"@FechaNacimiento={psr.FechaNacimiento}, @Email='{psr.Email}', @Telefono='{psr.Telefono}, @IdPlan={psr.IDPlan}' ";
+                context.Database.SqlQuery<Persona>($"PersonaUpdate {parametros}").FirstOrDefault();
+            }
+        }
+
+        protected void Insert(Persona psr)
+        {
+            using (var context = new AcademiaContext())
+            {
+                var parametros = $"@Id={psr.ID},@Nombre='{psr.Nombre}', @Apellido='{psr.Apellido}', @Direccion='{psr.Direccion}'," +
+                    $"@FechaNacimiento={psr.FechaNacimiento}, @Email='{psr.Email}', @Telefono='{psr.Telefono}, @IdPlan={psr.IDPlan}', @TipoPersona=2 ";
+                context.Database.SqlQuery<Persona>($"PersonaUpdate {parametros}").FirstOrDefault();
+            }
+        }
+
+        public void Save(Persona persona)
+        {
+            if (persona.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(persona.ID);
+            }
+            else if (persona.State == BusinessEntity.States.New)
+            {
+                this.Insert(persona);
+            }
+            else if (persona.State == BusinessEntity.States.Modified)
+            {
+                this.Update(persona);
+            }
+            persona.State = BusinessEntity.States.Unmodified;
+        }
+
+
+
         public Persona GetOneByLegajo(int Legajo)
         {
             Persona psr = new Persona();
@@ -119,103 +116,7 @@ namespace Data.Database
             return psr;
         }
 
-        public void Delete(int ID)
-        {
-            try
-            {
-                this.OpenConnection();
-                SqlCommand cmdAlumnos = new SqlCommand("DELETE FROM PERSONAS WHERE id_persona = @id",SqlConn);
-                cmdAlumnos.Parameters.Add("@id", SqlDbType.Int).Value = ID;
-                cmdAlumnos.ExecuteNonQuery();
-
-            }
-            catch(Exception Ex)
-            {
-                Exception ExcepcionControlada = new Exception("Error al recuperar alumno", Ex);
-                throw ExcepcionControlada;
-            }
-            finally
-            {
-                this.CloseConnection();
-            }
-        }
-
-        protected void Update(Persona psr)
-        {
-            try
-            {
-                this.OpenConnection();
-                SqlCommand cmdAlumnos = new SqlCommand("UPDATE PERSONAS set nombre = @nombre, apellido = @apellido, " +
-                    "direccion = @direccion, email = @email, telefono = @telefono , fecha_nac = @fechanac, legajo = @legajo, tipo_persona = @tipopersona, id_plan = @idplan where id_persona = @id "
-                    ,SqlConn);
-                cmdAlumnos.Parameters.Add("@id", SqlDbType.Int).Value = psr.ID;
-                cmdAlumnos.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = psr.Nombre;
-                cmdAlumnos.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = psr.Apellido;
-                cmdAlumnos.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = psr.Direccion;
-                cmdAlumnos.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = psr.Email;
-                cmdAlumnos.Parameters.Add("@telefono", SqlDbType.VarChar, 50).Value = psr.Telefono;
-                cmdAlumnos.Parameters.Add("@fechanac", SqlDbType.DateTime).Value = psr.FechaNacimiento;
-                cmdAlumnos.Parameters.Add("@legajo", SqlDbType.Int).Value = psr.Legajo;
-                cmdAlumnos.Parameters.Add("@tipopersona", SqlDbType.Int).Value = psr.TipoPersona;
-                cmdAlumnos.Parameters.Add("@idplan", SqlDbType.Int).Value = psr.IDPlan;
-                cmdAlumnos.ExecuteNonQuery();
-            }
-            catch(Exception Ex)
-            {
-                Exception ExcepcionManejada = new Exception("Error al actualizar alumno", Ex);
-                throw ExcepcionManejada;
-            }
-            finally
-            {
-                this.CloseConnection();
-            }
-        }
-
-        protected void Insert(Persona psr)
-        {
-            try
-            {
-                this.OpenConnection();
-                SqlCommand cmdAlumnos = new SqlCommand("INSERT INTO PERSONAS(nombre,apellido,direccion,email,telefono,fecha_nac,legajo,tipo_persona,id_plan)" +
-                    "values (@nombre,@apellido,@direccion,@email,@telefono,@fechanac,@legajo,@tipopersona,@idplan)", SqlConn);
-                cmdAlumnos.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = psr.Nombre;
-                cmdAlumnos.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = psr.Apellido;
-                cmdAlumnos.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = psr.Direccion;
-                cmdAlumnos.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = psr.Email;
-                cmdAlumnos.Parameters.Add("@telefono", SqlDbType.VarChar, 50).Value = psr.Telefono;
-                cmdAlumnos.Parameters.Add("@fechanac", SqlDbType.DateTime).Value = psr.FechaNacimiento;
-                cmdAlumnos.Parameters.Add("@legajo", SqlDbType.Int).Value = psr.Legajo;
-                cmdAlumnos.Parameters.Add("@tipopersona", SqlDbType.Int).Value = 2;
-                cmdAlumnos.Parameters.Add("@idplan", SqlDbType.Int).Value = 1;
-                cmdAlumnos.ExecuteNonQuery();
-            }
-            catch(Exception Ex)
-            {
-                Exception ExcepcionManejada = new Exception("Error al agregar alumno", Ex);
-                throw ExcepcionManejada;
-            }
-            finally
-            {
-                this.CloseConnection();
-            }
-        }
-
-        public void Save(Persona persona)
-        {
-            if (persona.State == BusinessEntity.States.Deleted)
-            {
-                this.Delete(persona.ID);
-            }
-            else if (persona.State == BusinessEntity.States.New)
-            {
-                this.Insert(persona);
-            }
-            else if (persona.State == BusinessEntity.States.Modified)
-            {
-                this.Update(persona);
-            }
-            persona.State = BusinessEntity.States.Unmodified;
-        }
+       
 
     }
 }
--- Data.Database/ProfesorAdapter.cs
+++ Data.Database/ProfesorAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Entities;
+using Data.Database.context;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -14,158 +15,49 @@ namespace Data.Database
     {
         public List<Persona> GetAll()
         {
-            List<Persona> profesores = new List<Persona>();
-            try
+            using (var context = new AcademiaContext())
             {
-                this.OpenConnection();
-                SqlCommand cmdProfesores = new SqlCommand("SELECT * FROM PERSONAS pe inner join planes p on p.id_plan = pe.id_plan where tipo_persona = 1", SqlConn);
-                SqlDataReader drProfesores = cmdProfesores.ExecuteReader();
-                while (drProfesores.Read())
-                {
-                    Persona psr = new Persona();
-                    psr.ID = (int)drProfesores["id_persona"];
-                    psr.Nombre = (string)drProfesores["nombre"];
-                    psr.Apellido = (string)drProfesores["apellido"];
-                    psr.TipoPersona = (int)drProfesores["tipo_persona"];
-                    psr.IDPlan = (int)drProfesores["id_plan"];
-                    psr.DescripcionPlan = (string)drProfesores["desc_plan"];
-                    psr.FechaNacimiento = (DateTime)drProfesores["fecha_nac"];
-                    psr.Direccion = drProfesores["direccion"] != DBNull.Value ? (string)drProfesores["direccion"] : null;
-                    psr.Telefono = drProfesores["telefono"] != DBNull.Value ? (string)drProfesores["telefono"] : null;
-                    psr.Legajo = drProfesores["legajo"] != DBNull.Value ? (int)drProfesores["legajo"] : 0;
-                    psr.Email = drProfesores["email"] != DBNull.Value ? (string)drProfesores["email"] : null;
-                    profesores.Add(psr);
-                }
-                drProfesores.Close();               
-                return profesores;
-            }
-            catch (Exception Ex)
-            {
-                Exception ExcepcionControlada = new Exception("Error al traer datos de profesores", Ex);
-                throw ExcepcionControlada;
-            }
-            finally
-            {
-                this.CloseConnection();
+                var parametros = "@TipoPersona=1";
+                return context.Database.SqlQuery<Persona>($"PersonaGetAll {parametros}").ToList();
             }
 
         }
 
         public Persona GetOne(int ID)
         {
-            Persona psr = new Persona();
-            try
+            using (var context = new AcademiaContext())
             {
-                this.OpenConnection();
-                SqlCommand cmdUsuario = new SqlCommand("SELECT * FROM PERSONAS WHERE id_persona=@id and tipo_persona = 1", SqlConn);
-                cmdUsuario.Parameters.Add("@id", SqlDbType.Int).Value = ID;
-                SqlDataReader drProfesores = cmdUsuario.ExecuteReader();
-                if (drProfesores.Read())
-                {
-
-                    psr.ID = (int)drProfesores["id_persona"];
-                    psr.Nombre = (string)drProfesores["nombre"];
-                    psr.Apellido = (string)drProfesores["apellido"];
-                    psr.FechaNacimiento = (DateTime)drProfesores["fecha_nac"];
-                    psr.TipoPersona = (int)drProfesores["tipo_persona"];
-                    psr.IDPlan = (int)drProfesores["id_plan"];
-                    psr.Direccion = drProfesores["direccion"] != DBNull.Value ? (string)drProfesores["direccion"] : null;
-                    psr.Telefono = drProfesores["telefono"] != DBNull.Value ? (string)drProfesores["telefono"] : null;
-                    psr.Legajo = drProfesores["legajo"] != DBNull.Value ? (int)drProfesores["legajo"] : 0;
-                    psr.Email = drProfesores["email"] != DBNull.Value ? (string)drProfesores["email"] : null;
-                }
-                drProfesores.Close();
-                return psr;
-            }
-            catch (Exception Ex)
-            {
-                Exception ExcepcionControlada = new Exception("Error al recuperar profesor", Ex);
-                throw ExcepcionControlada;
-            }
-            finally
-            {
-                this.CloseConnection();
+                var parametros = $"@Id={ID}";
+                return context.Database.SqlQuery<Persona>($"PersonaGetOne {parametros}").FirstOrDefault();
             }
         }
 
         public void Delete(int ID)
         {
-            try
+            using (var context = new AcademiaContext())
             {
-                this.OpenConnection();
-                SqlCommand cmdProfesores = new SqlCommand("DELETE FROM PERSONAS WHERE id_persona = @id", SqlConn);
-                cmdProfesores.Parameters.Add("@id", SqlDbType.Int).Value = ID;
-                cmdProfesores.ExecuteNonQuery();
-
-            }
-            catch (Exception Ex)
-            {
-                Exception ExcepcionControlada = new Exception("Error al recuperar profesores", Ex);
-                throw ExcepcionControlada;
-            }
-            finally
-            {
-                this.CloseConnection();
+                var parametros = $"@Id={ID}";
+                context.Database.SqlQuery<Persona>($"PersonaDelete {parametros}").FirstOrDefault();
             }
         }
 
         protected void Update(Persona psr)
         {
-            try
+            using (var context = new AcademiaContext())
             {
-                this.OpenConnection();
-                SqlCommand cmdProfesores = new SqlCommand("UPDATE PERSONAS set nombre = @nombre, apellido = @apellido, " +
-                    "direccion = @direccion, email = @email, telefono = @telefono , fecha_nac = @fechanac, legajo = @legajo, tipo_persona = @tipopersona, id_plan = @idplan where id_persona = @id "
-                    , SqlConn);
-                cmdProfesores.Parameters.Add("@id", SqlDbType.Int).Value = psr.ID;
-                cmdProfesores.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = psr.Nombre;
-                cmdProfesores.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = psr.Apellido;
-                cmdProfesores.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = psr.Direccion;
-                cmdProfesores.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = psr.Email;
-                cmdProfesores.Parameters.Add("@telefono", SqlDbType.VarChar, 50).Value = psr.Telefono;
-                cmdProfesores.Parameters.Add("@fechanac", SqlDbType.DateTime).Value = psr.FechaNacimiento;
-                cmdProfesores.Parameters.Add("@legajo", SqlDbType.Int).Value = psr.Legajo;
-                cmdProfesores.Parameters.Add("@tipopersona", SqlDbType.Int).Value = psr.TipoPersona;
-                cmdProfesores.Parameters.Add("@idplan", SqlDbType.Int).Value = psr.IDPlan;
-                cmdProfesores.ExecuteNonQuery();
-            }
-            catch (Exception Ex)
-            {
-                Exception ExcepcionManejada = new Exception("Error al actualizar profesor", Ex);
-                throw ExcepcionManejada;
-            }
-            finally
-            {
-                this.CloseConnection();
+                var parametros = $"@Id={psr.ID},@Nombre='{psr.Nombre}', @Apellido='{psr.Apellido}', @Direccion='{psr.Direccion}'," +
+                    $"@FechaNacimiento={psr.FechaNacimiento}, @Email='{psr.Email}', @Telefono='{psr.Telefono}, @IdPlan={psr.IDPlan}' ";
+                context.Database.SqlQuery<Persona>($"PersonaUpdate {parametros}").FirstOrDefault();
             }
         }
 
         protected void Insert(Persona psr)
         {
-            try
+            using (var context = new AcademiaContext())
             {
-                this.OpenConnection();
-                SqlCommand cmdProfesores = new SqlCommand("INSERT INTO PERSONAS(nombre,apellido,direccion,email,telefono,fecha_nac,legajo,tipo_persona,id_plan)" +
-                    "values (@nombre,@apellido,@direccion,@email,@telefono,@fechanac,@legajo,@tipopersona,@idplan)", SqlConn);
-                cmdProfesores.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = psr.Nombre;
-                cmdProfesores.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = psr.Apellido;
-                cmdProfesores.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = psr.Direccion;
-                cmdProfesores.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = psr.Email;
-                cmdProfesores.Parameters.Add("@telefono", SqlDbType.VarChar, 50).Value = psr.Telefono;
-                cmdProfesores.Parameters.Add("@fechanac", SqlDbType.DateTime).Value = psr.FechaNacimiento;
-                cmdProfesores.Parameters.Add("@legajo", SqlDbType.Int).Value = psr.Legajo;
-                cmdProfesores.Parameters.Add("@tipopersona", SqlDbType.Int).Value = 1;
-                cmdProfesores.Parameters.Add("@idplan", SqlDbType.Int).Value = psr.IDPlan;
-                cmdProfesores.ExecuteNonQuery();
-            }
-            catch (Exception Ex)
-            {
-                Exception ExcepcionManejada = new Exception("Error al agregar profesor", Ex);
-                throw ExcepcionManejada;
-            }
-            finally
-            {
-                this.CloseConnection();
+                var parametros = $"@Id={psr.ID},@Nombre='{psr.Nombre}', @Apellido='{psr.Apellido}', @Direccion='{psr.Direccion}'," +
+                    $"@FechaNacimiento={psr.FechaNacimiento}, @Email='{psr.Email}', @Telefono='{psr.Telefono}, @IdPlan={psr.IDPlan}', @TipoPersona=1 ";
+                context.Database.SqlQuery<Persona>($"PersonaUpdate {parametros}").FirstOrDefault();
             }
         }
 
